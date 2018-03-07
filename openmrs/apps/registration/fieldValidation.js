d73b85b6d0ac27ddd6dcfee00b6c485974255456
Edward Molina <eamolina@unicauca.edu.co>
2018-03-07 08:56:18 -0500

    Neuromotic v1

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -5,16 +5,11 @@ Bahmni.Registration.customValidator = {
         },
         errorMessage: "REGISTRATION_AGE_ERROR_KEY"
     },
-    "Telephone Number": {
+    "Correo electrónico": {
         method: function (name, value, personAttributeDetails) {
-            return value && value.length> 6;
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(value.toLowerCase());
         },
-        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
-    },
-    "caste": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/^\w+$/);
-        },
-        errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
+        errorMessage: 'Ingrese un correo electrónico válido.'
     }
 };
